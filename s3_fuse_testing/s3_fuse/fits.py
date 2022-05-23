5aeac8d69990866840c1e8f8962e3051cd7c1b00
michael <mstclair@millionconcepts.com>
2022-05-23 16:22:26 -0400

    draft of ps1-slicing notebook; linting

--- s3_fuse_testing/s3_fuse/fits.py
+++ s3_fuse_testing/s3_fuse/fits.py
@@ -1,9 +1,13 @@
 """utility functions for occasional FITS manipulation"""
 import os
 import re
+from itertools import product
+from operator import add, sub
 from typing import Sequence, Literal
 
+import astropy.wcs
 from cytoolz import keyfilter
+import numpy as np
 
 
 def make_tiled_galex_object(
@@ -69,3 +73,113 @@ def imsz_from_header(header):
         lambda k: re.match(rf"{key_type}\d", k), dict(header)
     )
     return tuple(axis_entries.values())
+
+
+# the next four functions are vendored from gPhoton.coords.wcs
+def translate_pc_keyword(keyword: str):
+    """
+    convert old-style fits wcs transformation keywords. this is not strictly
+    necessary for any GALEX products, but is useful for some data fusion
+    applications.
+    """
+    # i suppose this will fail for headers with hundreds
+    # of dimensions -- they may not exist, and deserve special-purpose
+    # code if they do
+    if not keyword.startswith("PC0"):
+        return keyword
+    return keyword.replace("PC00", "PC").replace("00", "_")
+
+
+def extract_wcs_keywords(header):
+    """
+    header formatting and WCS keyword handling can make astropy.wcs upset,
+    it handles validation and fixes gracefully, but not quickly. faster
+    to trim irrelevant keywords and fix old-style ones before feeding them to
+    astropy.wcs.
+    """
+    wcs_words = ('CTYPE', 'CRVAL', 'CRPIX', 'CDELT', 'NAXIS', 'PC')
+    return {
+        translate_pc_keyword(k): header[k] for k in header.keys()
+        if any([k.startswith(w) for w in wcs_words])
+    }
+
+
+def corners_of_a_square(ra, dec, side_length):
+    """
+    corners of a square centered at ra, dec with side length side_length
+    in the order: upper right, lower right, upper left, lower left
+    """
+    return [
+        (op1(ra, side_length / 2), op2(dec, side_length / 2))
+        for op1, op2 in product((add, sub), (add, sub))
+    ]
+
+
+def sky_box_to_image_box(corners, system):
+    """
+    get image coordinates that correspond to a sky-coordinate square
+    with specified corners (in whatever units wcs axes 1 and 2
+    are in, most likely degrees)
+    """
+    cuts = system.world_to_pixel_values(
+        np.array(corners)[:, 0], np.array(corners)[:, 1]
+    )
+    return tuple(
+        map(int, (cuts[0].min(), cuts[0].max(), cuts[1].min(), cuts[1].max()))
+    )
+
+
+def make_wcs(
+    skypos: Sequence,
+    pixsz: float = 0.000416666666666667,
+    imsz: Sequence[int] = (3200, 3200),
+    proj=("RA---TAN", "DEC--TAN")
+) -> astropy.wcs.WCS:
+    """
+    makes a WCS object from passed center ra/dec, scale, and image size
+    parameters. by default, uses the nominal image size and pixel scale
+    values from the internal mission intensity map products, and a gnomonic
+    projection.
+    """
+    wcs = astropy.wcs.WCS(naxis=2)
+    wcs.wcs.cdelt = np.array([-pixsz, pixsz])
+    wcs.wcs.ctype = list(proj)
+    wcs.wcs.crpix = [(imsz[1] / 2.0) + 0.5, (imsz[0] / 2.0) + 0.5]
+    wcs.wcs.crval = skypos
+    return wcs
+
+
+def make_bounding_wcs(
+    radec: np.ndarray,
+    pixsz: float = 0.000416666666666667,
+    proj=("RA---TAN", "DEC--TAN")
+) -> astropy.wcs.WCS:
+    """
+    makes a WCS solution for a given range of ra/dec values
+    by default, assumes gnomonically-projected ra/dec values; scales ra bounds
+    to approximate distortion in pixel size
+    radec: n x 2 array with ra in first column and dec in second
+    pixsz: size of returned WCS's pixels in square degrees;
+    defaults to degree-per-pixel scale set in gPhoton.constants.DEGPERPIXEL
+    """
+    import math
+    real_ra = radec[:, 0][np.isfinite(radec[:, 0])]
+    real_dec = radec[:, 1][np.isfinite(radec[:, 1])]
+    ra_range = real_ra.min(), real_ra.max()
+    dec_range = real_dec.min(), real_dec.max()
+    # handle viewports in which ra wraps around 360
+    if ra_range[1] - ra_range[0] > 350:
+        real_ra[real_ra > 180] -= 360
+        ra_range = real_ra.min(), real_ra.max()
+    # WCS center pixel in sky coordinates
+    ra0, dec0 = (np.mean(ra_range), np.mean(dec_range))
+    ra0 = ra0 if ra0 > 0 else ra0 + 360
+    # scale ra-axis pixel size using cos(declination) to approximate
+    # ra-direction distortion introduced by gnomonic projection
+    ra_offset = (ra_range[1] - ra_range[0]) * math.cos(math.radians(dec0))
+    imsz = (
+        int(np.ceil((dec_range[1] - dec_range[0]) / pixsz)),
+        int(np.ceil(ra_offset / pixsz)),
+    )
+    return make_wcs((ra0, dec0), imsz=imsz, pixsz=pixsz, proj=proj)
+
---
+++ s3_fuse_testing/s3_fuse/ps1_utils.py
@@ -0,0 +1,89 @@
+"""
+utilities for interacting with PS1 catalogs, files, and services.
+"""
+from pathlib import Path
+
+import astropy
+import astropy.wcs
+import fitsio
+import pyarrow as pa
+import pyarrow.compute as pac
+from cytoolz import first
+
+from s3_fuse.fits import (
+    extract_wcs_keywords, corners_of_a_square, sky_box_to_image_box
+)
+
+
+def ps1_stack_path(proj_cell, sky_cell, band):
+    """
+    construct relative path for a PS1 stack image by projection cell,
+    sky cell, and band.
+    """
+    proj_cell = str(proj_cell).zfill(4)
+    sky_cell = str(sky_cell).zfill(3)
+    return (
+        f"/rings.v3.skycell/{proj_cell}/{sky_cell}/"
+        f"rings.v3.skycell.{proj_cell}.{sky_cell}.stk.{band}.unconv.fits"
+    )
+
+
+def prune_ps1_catalog(catalog_table, test_table):
+    """
+    support function for testing subsets of the PS1 archive.
+    filter a pyarrow table of PS1 objects, retaining only those
+    objects that fall into the projection/sky cells defined in test_table.
+    """
+    # i chose this convoluted-looking technique because pyarrow won't join
+    # if there's a list data type, and the equivalent operation is slow in
+    # pandas
+    proj_cell_tables = []
+    for proj_cell in pac.unique(test_table['proj_cell']):
+        allowed_skies = pac.unique(
+            test_table.filter(
+                pac.equal(test_table['proj_cell'], proj_cell)
+            )['sky_cell']
+        )
+        catslice = catalog_table.filter(
+            pac.equal(catalog_table['proj_cell'], proj_cell)
+        )
+        proj_cell_tables.append(
+            catslice.filter(pac.is_in(catslice['sky_cell'], allowed_skies))
+        )
+    return pa.concat_tables(proj_cell_tables)
+
+
+def get_ps1_cutouts(
+    target, bands, side_length, data_root, stat, watch
+):
+    band_cutouts = {}
+    ps1_wcs, coords = None, None
+    for band in bands:
+        # note that I exactly copied the ps1 file tree structure for this
+        # test s3 deployment
+        path = ps1_stack_path(target['proj_cell'], target['sky_cell'], band)
+        path = f"{data_root}/ps1{path}"
+        hdul = fitsio.FITS(path)
+        # all bands of a ps1 stack image should have the same wcs, don't
+        # waste time projecting stuff twice
+        if ps1_wcs is None:
+            print("... planning cuts on PS1 images ...")
+            ps1_wcs = astropy.wcs.WCS(
+                extract_wcs_keywords(hdul[1].read_header())
+            )
+            corners = corners_of_a_square(
+                target['ra'], target['dec'], side_length
+            )
+            coords = sky_box_to_image_box(corners, ps1_wcs)
+            watch.click()
+        print(f"slicing data from {Path(path).name}")
+        band_cutouts[band] = hdul[1][
+             coords[2]:coords[3] + 1, coords[0]:coords[1] + 1
+        ]
+        watch.click()
+        stat.update()
+        print(
+            f"{round(first(stat.interval.values()) / 1024 ** 2)} "
+            f"MB transferred"
+        )
+    return band_cutouts, ps1_wcs
